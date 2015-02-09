5504753cfae24efcc89d0a65eebc30a2f1442fcf
swlewis <swl5nw@virginia.edu>
2015-02-09 15:31:59 -0500

    php division exercise

--- exercises_2.php
+++ exercises_2.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-		<title>Seconds to Years</title>
+		<title>Multiply</title>
 </head>
 <body>
 	<?php
@@ -16,3 +16,22 @@
 	<p><?php echo multiply (o, 4); ?></p>
 	<p><?php echo multiply (900, 32); ?></p>
 	<p><?php echo multiply (299999, 23); ?></p>
+	
+	<?php
+
+	function divide ($x, $y)
+	{
+	if ($y == 0) {
+		$y = $x;
+	}
+	return $x / $y;
+	}
+
+	?>
+
+	<p><?php echo divide (4, 2); ?></p>
+	<p><?php echo divide (900, 32); ?></p>
+	<p><?php echo divide (0, 4); ?></p>
+	<p><?php echo divide (4, 0); ?></p>
+
+	<?php
