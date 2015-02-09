842d22464bb6ae0e29268ed5af8565fdf4a4e25c
swlewis <swl5nw@virginia.edu>
2015-02-09 14:16:54 -0500

    git commit -m "php multiplication exercise"

---
+++ exercises_2.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+		<title>Seconds to Years</title>
+</head>
+<body>
+	<?php
+
+	function multiply ($x, $y)
+	{
+		return $x * $y;
+	}
+
+	?>
+
+	<p><?php echo multiply (4, 2); ?></p>
+	<p><?php echo multiply (o, 4); ?></p>
+	<p><?php echo multiply (900, 32); ?></p>
+	<p><?php echo multiply (299999, 23); ?></p>
